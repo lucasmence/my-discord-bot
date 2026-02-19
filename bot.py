38e7176a0b79c502b8f4f4680e7692a240755810
Mence <lucas21021996@gmail.com>
2026-02-18 22:15:55 -0300

    Added bot.py

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+import os
+import re
+
+token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.presences = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Success: {bot.user}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    instagramRegex = r"(https?://(?:www\.)?instagram\.com/(?:p|reels|reel)/([^/?#&]+))"
+    match = re.search(instagramRegex, message.content)
+
+    if match:
+        linkNew = link_original.replace("instagram.com", "kkinstagram.com")
+            
+        try:
+            await message.delete()
+        except discord.Forbidden:
+            return
+
+        content = f"[{message.author.display_name}]({linkNew})"
+            
+        await message.channel.send(content)
+
+    await bot.process_commands(message)
+
+if token:
+    bot.run(token)
+else:
+    print("Error: DISCORD_TOKEN not found!")
